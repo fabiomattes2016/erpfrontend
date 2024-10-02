b84963d1ca3d6c2d4141dee75da4e2f93ab941bd
Fábio Mattes <fabiomattes2007@gmail.com>
2024-10-02 20:37:09 -0300

    Componente PermisionList

---
+++ src/components/PermissionsList/index.tsx
@@ -0,0 +1,42 @@
+import { Checkbox, FormControlLabel, FormGroup } from "@mui/material";
+import { ChangeEvent } from "react";
+import { PermissionDetail } from "src/models/permission";
+
+type Props = {
+    permissionsData: PermissionDetail[];
+    selectedPermissions: number[];
+    setSelectedPermissions: (value: number[]) => void;
+}
+
+function PermissionsList({permissionsData, selectedPermissions, setSelectedPermissions}: Props) {
+    const handleChange = (event: ChangeEvent<HTMLInputElement>, permissionId: number) => {
+        const {checked} = event.target;
+
+        if (checked) {
+            setSelectedPermissions([...selectedPermissions, permissionId]);
+        } else {
+            setSelectedPermissions(selectedPermissions.filter(fId => fId != permissionId));
+        }
+    }
+
+    return (
+        <FormGroup>
+            {permissionsData.map((item) => {
+                <FormControlLabel 
+                    key={item.id} 
+                    control={
+                        <Checkbox 
+                            checked={
+                                selectedPermissions.find((id) => id == item.id) != undefined
+                            }
+                            onChange={(e) => handleChange(e, item.id)}
+                        />
+                    }
+                    label={item.name}
+                />
+            })}
+        </FormGroup>
+    );
+}
+
+export default PermissionsList;
